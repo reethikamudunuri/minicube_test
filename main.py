5f3d62425728a5c2695c1eb1f52a14bacf250990
Reethika Mudunuri <mudunurireethika95@gmail.com>
2021-11-14 22:29:06 +0100

    commits

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from kube import Pod
+from rules import ApplyRules
+import pprint
 
 
 
 
 def main():
     pod = Pod()
+    pods = pod_api.build_pod_details()
+    rules = ApplyRules()
+    result = rules.validate(
+        pods=pods
+    )
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(result)
 
 
 if __name__ == '__main__':
