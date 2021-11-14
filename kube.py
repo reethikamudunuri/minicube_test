92a84c9100a0387a9ace497a505c8f9f34de432c
Reethika Mudunuri <mudunurireethika95@gmail.com>
2021-11-14 22:06:36 +0100

    commits

--- kube.py
+++ kube.py
@@ -17,3 +17,20 @@ class Pod(object):
 
     def __get_start_time(self, pod):
         return pod.status.start_time
+
+    def build_pod_details(self):
+        pod_details = []
+        for pod in self.pods.items:
+            name = pod.metadata.name
+            images = self.__get_images(pod=pod)
+            labels = self.__get_labels(pod=pod)
+            recent_start_time = self.__get_start_time(pod=pod)
+            pod_details.append(
+                Pod(
+                    name=name,
+                    images=images,
+                    labels=labels,
+                    recent_start_time=recent_start_time
+                )
+            )
+        return pod_details
